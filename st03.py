580c42d64cfbf4cf26f7bff6b5f7d1f8d16329bf
KMN43 <42774827+KMN43@users.noreply.github.com>
2022-11-30 13:31:10 +0100

    Add files via upload

--- st03.py
+++ st03.py
@@ -8,6 +8,7 @@ df = pd.read_csv('ExSDReport.csv')
 
 st.set_page_config(
     page_title="Live Dashboard",
+    page_icon=":banana:",
     layout="wide"
 )
 
@@ -21,9 +22,9 @@ df1 = df[df['Process Path'] == PPH_filter]
 PPhs_list = pd.unique(df['Process Path'])
 
 df_total = df.groupby(['Process Path', 'Work Pool']).agg({'Quantity': 'sum'}).reset_index()
-pip
-PP_SM= df_total.loc[(df_total['Process Path'] == "PPSingleMedium") & (df_total['Work Pool'] == "PickingPicked")]['Quantity']
-PP_MM= df_total.loc[(df_total['Process Path'] == "PPMultiMedium") & (df_total['Work Pool'] == "PickingPicked")]['Quantity']
+PP_SM = df_total.loc[(df_total['Process Path'] == "PPSingleMedium") & (df_total['Work Pool'] == "PickingPicked")]['Quantity']
+PP_MM = df_total.loc[(df_total['Process Path'] == "PPMultiMedium") & (df_total['Work Pool'] == "PickingPicked")]['Quantity']
+C7 = df_total.loc[df_total['Work Pool'] == "Scanned"]['Quantity']
 
 for seconds in range(200):
     
@@ -31,7 +32,36 @@ for seconds in range(200):
         kp1, kp2, kp3 = st.columns(3)
         kp1.metric(label="Picking Picked Single", value= PP_SM+np.random.randint(low=0,high=100))
         kp2.metric(label="Picking Picked Multi", value=PP_MM+np.random.randint(low=0,high=100))
+        kp3.metric(label="Scanned", value=C7+np.random.randint(low=0,high=100))
+        
+    st.markdown("""
+    <style>
+    div[data-testid="metric-container"] {
+   background-color: rgba(28, 131, 225, 0.1);
+   border: 1px solid rgba(28, 131, 225, 0.1);
+   padding: 5% 5% 5% 10%;
+   border-radius: 1px;
+   color: rgb(30, 103, 119);
+   overflow-wrap: break-word;
+   
+    }
 
+    /* breakline for metric text         */
+    div[data-testid="metric-container"] > label[data-testid="stMetricLabel"] > div {
+   overflow-wrap: break-word;
+   white-space: break-spaces;
+   color: white;
+   font-size: 20px;
+    }
+    
+    div[data-testid="stMetricValue"] {
+    font-size: 100px;
+    color: white;
+    }
+    </style>
+    """
+    , unsafe_allow_html=True)
+    
 
     
     time.sleep(1)
